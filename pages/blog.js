d6c6ce6362e34608692d706f73cc8df732a11274
sid-talesara <2020pcecssiddharth207@poornima.org>
2023-03-15 11:12:47 +0530

    Blog section added

--- pages/blog.js
+++ pages/blog.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import styles from '../styles/Blog.module.css' 
-import Link from 'next/link';
+/* eslint-disable @next/next/link-passhref */
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 // Step 1: Collect all the files from blogdata directory
 // Step 2: Iterate through the and Display them
 
-const Blog = () => {
-  return <div className={styles.container}>
-    <main className={styles.main}>
-  <div>
-    <Link href={'/blogpost/learn-javascript'}>
-    <h3 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h3></Link>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
-  <div className="blogItem">
-    <h3>How to learn JavaScript in 2022?</h3>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
-  <div className="blogItem">
-    <h3>How to learn JavaScript in 2022?</h3>
-    <p>JavaScript is the language used to design logic for the web</p>
-  </div>
-  </main>
-</div>
+const Blog = (props) => {
+  const [blogs, setBlogs] = useState(props.allBlogs);
+  console.log(props);
+  // This function will fetch in client side
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/api/blogs")
+  //     .then((a) => {
+  //       return a.json();
+  //     })
+  //     .then((data) => {
+  //       setBlogs(data);
+  //     });
+  // }, []);
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        {blogs.map((blogitem, index) => {
+          return (
+            <div key={index}>
+              <Link href={`/blogpost/${blogitem.slug}`}>
+                <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
+              </Link>
+              <p className={styles.blogitempara}>
+                {" "}
+                {blogitem.content.substr(0, 140)}...
+              </p>
+            </div>
+          );
+        })}
+      </main>
+    </div>
+  );
 };
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  let data = await fetch("http://localhost:3000/api/blogs");
+  let allBlogs = await data.json();
+
+  return {
+    props: { allBlogs }, // will be passed to the page component as props
+  };
+}
